69f4abc80a96b1ac06dbef15a5260a334ed24126
Jason Haenlin <jasonjas5252@gmail.com>
2019-03-17 20:57:04 +0100

    GH-3 change la police et ajoute une marge sur les côtés modifie le dropdown pour avoir une largeur minimum adapté à l'option sélectionné.

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  public icone = 'fas fa-home';
-  public arrow_icone = 'fas fa-angle-down';
+  public icon = 'fas fa-home';
+  public arrow_icon = 'fas fa-angle-down';
 
   constructor() { }
 
