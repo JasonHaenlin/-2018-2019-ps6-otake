0716b5ba9e0f6dbb33b122b30c194440974c61d2
Jason Haenlin <jasonjas5252@gmail.com>
2019-03-18 18:47:16 +0100

    GH-3 menu déroulant pour les major et zone geo get au service des valeurs pour le moment mocked

--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,4 +1,8 @@
+import { Major } from './../../models/Major';
+import { GeographicalArea } from './../../models/GeographicalArea';
 import { Component, OnInit } from '@angular/core';
+import { UniversityService } from './../../services/university/university.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -10,9 +14,14 @@ export class HeaderComponent implements OnInit {
   public icon = 'fas fa-home fa-2x';
   public arrow_icon = 'fas fa-angle-down';
 
-  constructor() { }
+  public areaList$: Observable<GeographicalArea[]>;
+  public majorList$: Observable<Major[]>;
+
+  constructor(private universityService: UniversityService) { }
 
   ngOnInit() {
+    this.areaList$ = this.universityService.getGeographicalAreas();
+    this.majorList$ = this.universityService.getMajors();
   }
 
 }
