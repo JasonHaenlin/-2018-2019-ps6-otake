9b810b22879b944834dbcbc5a9867c27f4c0c153
maxcastellano <maxime.castellano@etu.univ-cotedazur.fr>
2019-03-24 20:56:05 +0100

    Amélioration CSS du selector

--- front/src/app/utility/section-selector/section-selector.component.spec.ts
+++ front/src/app/utility/section-selector/section-selector.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SectionSelectionComponent } from './section-selector.component';
+import { SectionSelectorComponent } from './section-selector.component';
 
 describe('SectionSelectionComponent', () => {
-  let component: SectionSelectionComponent;
-  let fixture: ComponentFixture<SectionSelectionComponent>;
+  let component: SectionSelectorComponent;
+  let fixture: ComponentFixture<SectionSelectorComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SectionSelectionComponent ]
+      declarations: [ SectionSelectorComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SectionSelectionComponent);
+    fixture = TestBed.createComponent(SectionSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
