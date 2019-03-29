d667b78ed7444016e28a4b30bd788a8c4ad6f04c
Jason Haenlin <jasonjas5252@gmail.com>
2019-03-29 14:16:31 +0100

    GH-22 valeurs pour study-departement + exchange-university

--- back/models/major.js
+++ back/models/major.js
@@ -7,11 +7,12 @@ module.exports = new BaseModel('major', t => {
   t.string('icon', 100);
 },
 [
-  { id: 0, title: 'Informatique', shorthand: 'SI' },
-  { id: 1, title: 'Génie de l\'eau', shorthand: 'GE' },
-  { id: 2, title: 'Bâtiments', shorthand: 'BAT' },
-  { id: 3, title: 'Electronique', shorthand: 'ELEC' },
-  { id: 4, title: 'Génie biologique', shorthand: 'GB' },
-  { id: 5, title: 'Math.App. & Modélisation', shorthand: 'MAM' },
-  { id: 6, title: 'Elec. & info.industrielle', shorthand: 'EII' }
+  { id: 0, title: 'Tous', shorthand: 'ALL' },
+  { id: 1, title: 'Informatique', shorthand: 'SI' },
+  { id: 2, title: 'Génie de l\'eau', shorthand: 'GE' },
+  { id: 3, title: 'Bâtiments', shorthand: 'BAT' },
+  { id: 4, title: 'Electronique', shorthand: 'ELEC' },
+  { id: 5, title: 'Génie biologique', shorthand: 'GB' },
+  { id: 6, title: 'Math.App. & Modélisation', shorthand: 'MAM' },
+  { id: 7, title: 'Elec. & info.industrielle', shorthand: 'EII' }
 ]);
