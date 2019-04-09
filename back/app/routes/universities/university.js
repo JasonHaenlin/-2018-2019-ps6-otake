1aa59e46abfb0490ea41992468ae331534c8c7bd
Shuangshuang Chen <chenss961214@gmail.com>
2019-04-09 08:53:12 +0200

    #32 Requêtes aux backend pour filtrer

--- back/app/routes/universities/university.js
+++ back/app/routes/universities/university.js
@@ -3,6 +3,6 @@ const { university } = require('../../controller/universities');
 
 // using the database
 exports.getUniversities = async (req, res) => {
-  const universities = await university.getUniversitiesShortInfo();
+  const universities = await university.getUniversitiesShortInfo(req.query.area, req.query.language, req.query.major);
   resHandler.yahResponse(res, universities);
 };
