6ba8c2bf292a57e8ec0a26d6430f3a1cdc2fad81
Jason Haenlin <jasonjas5252@gmail.com>
2019-04-11 16:47:52 +0200

    GH-51 change le modèle pour cost_of_living

--- front/src/models/University.ts
+++ front/src/models/University.ts
@@ -5,7 +5,7 @@ export interface University {
   name?: string;
   admission_rate?: number;
   type_of_file?: string;
-  cost_of_living?: string;
+  cost_of_living?: number;
   nb_of_place?: number;
   small_picture?: string;
   city?: string;
