4ae3657add6c7f8c00f299f8d081eb98c24e3abf
Jason Haenlin <jasonjas5252@gmail.com>
2019-04-14 13:09:51 +0200

    GH-38 applique une date qui change sur les liens des dossiers selon les années

--- front/src/app/footer/footer.component.ts
+++ front/src/app/footer/footer.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public schoolYear = '';
+
   constructor() { }
 
   ngOnInit() {
+    this.setfilesYearSession();
   }
 
+  private setfilesYearSession() {
+    const currentDate = new Date();
+    let admissionyear = currentDate.getFullYear();
+    if (currentDate.getMonth() > 8) { admissionyear++; }
+    this.schoolYear = `${admissionyear}-${++admissionyear}`;
+  }
 }
