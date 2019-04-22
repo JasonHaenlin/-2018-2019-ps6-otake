15328a6fb4a63ab60cfd1909524a5c89acb8b980
Jason Haenlin <jasonjas5252@gmail.com>
2019-04-22 21:09:31 +0200

    GH-52 améliore un peu le css des contacts

--- back/models/supervisor.js
+++ back/models/supervisor.js
@@ -7,11 +7,44 @@ module.exports = new BaseModel('supervisor', t => {
   t.string('email', 50);
 },
 [
-  { id: 0, name: 'Contact 1', title: 'Responsable', email: 'contact-1@mail.com' },
-  { id: 1, name: 'Contact 2', title: 'Responsable', email: 'contact-2@mail.com' },
-  { id: 2, name: 'Contact 3', title: 'Responsable', email: 'contact-3@mail.com' },
-  { id: 3, name: 'Contact 4', title: 'Responsable', email: 'contact-4@mail.com' },
-  { id: 4, name: 'Contact 5', title: 'Administrateur', email: 'contact-5@mail.com' },
-  { id: 5, name: 'Contact 6', title: 'Administrateur', email: 'contact-6@mail.com' },
-  { id: 6, name: 'Contact 7', title: 'Administrateur', email: 'contact-7@mail.com' }
+  {
+    id: 0, name: 'Maria-Paola Santisi d’Avila', title: 'Correspondant RI - GE',
+    email: 'maria-paola.santisi-davila@polytech.unice.fr'
+  },
+  {
+    id: 1, name: 'Sébastien Bilavarn', title: 'Correspondant RI - ELEC',
+    email: 'sébastien.bilavarn@polytech.unice.fr'
+  },
+  {
+    id: 2, name: 'Any Cupo', title: 'Correspondant RI - GB',
+    email: 'any.cupo@polytech.unice.fr'
+  },
+  {
+    id: 3, name: 'Eric Macia', title: 'Correspondant RI - GB',
+    email: 'eric.macia@polytech.unice.fr'
+  },
+  {
+    id: 4, name: 'Pierre Brigode', title: 'Correspondant RI - GE',
+    email: 'pierre.brigode@polytech.unice.fr'
+  },
+  {
+    id: 5, name: 'Abderrahmane Habbal', title: 'Correspondant RI - MAM',
+    email: 'abderrahmane.habbal@polytech.unice.fr'
+  },
+  {
+    id: 6, name: 'Anne-Marie Pinna', title: 'Correspondant RI - SI',
+    email: 'anne-marie.pinna@polytech.unice.fr'
+  },
+  {
+    id: 7, name: 'Julie Maiffret', title: 'Responsable BRI',
+    email: 'bri@polytech.unice.fr'
+  },
+  {
+    id: 8, name: 'Joanna Winchcombe', title: 'Responsable BRI',
+    email: 'bri@polytech.unice.fr'
+  },
+  {
+    id: 9, name: 'Jason Haenlin', title: 'Administrateur',
+    email: 'contact@otakedev.com'
+  }
 ]);
