3597031378a40ec1ccfeba0ec651a7a88650dc20
Shuangshuang Chen <chenss961214@gmail.com>
2019-04-22 17:48:37 +0200

    #52 Faire des mocks pour tester le composant.

---
+++ front/src/mocks/Supervisor.mock.ts
@@ -0,0 +1,22 @@
+import { Supervisor } from "../models/Supervisor";
+
+export const SUPERVISOR_MOCKED: Supervisor[] = [
+  {
+    id: 0,
+    name: 'Contact 1',
+    title: 'Responsable',
+    email: 'contact-1@mail.com'
+  },
+  {
+    id: 1,
+    name: 'Contact 2',
+    title: 'Responsable',
+    email: 'contact-2@mail.com'
+  },
+  {
+    id: 2,
+    name: 'Contact 3',
+    title: 'Responsable',
+    email: 'contact-3@mail.com'
+  },
+];
