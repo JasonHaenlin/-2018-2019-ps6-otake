c1ddd9f11fce8ce2c71bdcb271797722b5ffaee2
Shuangshuang Chen <chenss961214@gmail.com>
2019-05-07 08:44:24 +0200

    #65 Changer les images des filtres pour les pays.

--- back/models/geographical-area.js
+++ back/models/geographical-area.js
@@ -7,10 +7,10 @@ module.exports = new BaseModel('geographical_area', t => {
   t.string('icon', 255);
 },
 [
-  { id: 0, area_name: 'Europe', shorthand: 'EU', icon: 'https://cdn.onlinewebfonts.com/svg/img_169113.png' },
-  { id: 1, area_name: 'Asie', shorthand: 'AS', icon: 'https://cdn.onlinewebfonts.com/svg/img_169114.png' },
-  { id: 2, area_name: 'Amérique du Nord', shorthand: 'NA', icon: 'https://cdn.onlinewebfonts.com/svg/img_169105.png' },
-  { id: 3, area_name: 'Amérique du Sud', shorthand: 'SA', icon: 'https://cdn.onlinewebfonts.com/svg/img_169112.png' },
-  { id: 4, area_name: 'Afrique', shorthand: 'AF', icon: 'https://cdn.onlinewebfonts.com/svg/img_169108.png' }
+  { id: 0, area_name: 'Europe', shorthand: 'EU', icon: 'https://otakedev.com/share/images/areas/EU.png' },
+  { id: 1, area_name: 'Asie', shorthand: 'AS', icon: 'https://otakedev.com/share/images/areas/ASIA.png' },
+  { id: 2, area_name: 'Amérique du Nord', shorthand: 'NA', icon: 'https://otakedev.com/share/images/areas/AN.png' },
+  { id: 3, area_name: 'Amérique du Sud', shorthand: 'SA', icon: 'https://otakedev.com/share/images/areas/AS.png' },
+  { id: 4, area_name: 'Afrique', shorthand: 'AF', icon: 'https://otakedev.com/share/images/areas/AF.png' }
   // { id: 5, area_name: 'Océanie', shorthand: 'OC', icon: 'https://cdn.onlinewebfonts.com/svg/img_169107.png' }
 ]);
