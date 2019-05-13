a28235010d4941f1186bc5f2f8740f3a03698fbd
Betsara MARCELLIN <betsara.marcellin@etu.univ-cotedazur.fr>
2019-05-13 14:18:27 +0200

    code clean accordion

--- front/src/app/utility/accordion/accordion.component.ts
+++ front/src/app/utility/accordion/accordion.component.ts
@@ -7,32 +7,32 @@ import { trigger, state, style, transition, animate, group } from '@angular/anim
   animations: [
     trigger('openClose', [
       state('open', style({
-        'height': '*', 'opacity': '1', 'visibility': 'visible'
+        height: '*', opacity: '1', visibility: 'visible'
       })),
       state('close', style({
-        'height': '0', 'opacity': '0', 'visibility': 'hidden'
+        height: '0', opacity: '0', visibility: 'hidden'
       })),
       transition('open => close', [group([
         animate('400ms ease-in-out', style({
-          'opacity': '0'
+          opacity: '0'
         })),
         animate('600ms ease-in-out', style({
-          'height': '0'
+          height: '0'
         })),
         animate('700ms ease-in-out', style({
-          'visibility': 'hidden'
+          visibility: 'hidden'
         }))
       ]
       )]),
       transition('close => open', [group([
         animate('1ms ease-in-out', style({
-          'visibility': 'visible'
+          visibility: 'visible'
         })),
         animate('600ms ease-in-out', style({
-          'height': '*'
+          height: '*'
         })),
         animate('800ms ease-in-out', style({
-          'opacity': '1'
+          opacity: '1'
         }))
       ]
       )])
