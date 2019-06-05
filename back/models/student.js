c95ff6fed6170d18687df305fab09f06ea0f3b41
Jason Haenlin <jasonjas5252@gmail.com>
2019-06-05 18:16:36 +0200

    GH-112 ajoute un student

--- back/models/student.js
+++ back/models/student.js
@@ -11,5 +11,9 @@ module.exports = new BaseModel('student', t => {
   {
     id: 'bc06b188-5f63-4bdb-bd1e-481efa8e91a3',
     first_name: 'John', last_name: 'Doe', major_id: 1
+  },
+  {
+    id: 'c8d1ac72-b399-4519-8445-7960c60841b6',
+    first_name: 'Maxime', last_name: 'Castellano', major_id: 2
   }
 ]);
